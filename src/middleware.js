1b9d18b6ea0f5aaa9e3f09f598d8d0d7e8a2ba68
ocircleo <salmanhossain11222626@gmail.com>
2026-01-08 16:53:46 +0600

    middlewere code patched

--- src/middleware.js
+++ src/middleware.js
@@ -5,13 +5,11 @@ import { jwtVerify, base64url } from "jose";
 const secret = base64url.decode(process.env.JWT_REFRESH_TOKEN_SECRET);
 
 export const middleware = async (request) => {
-
-  let accessToken = request.cookies.get("access_token")?.value;
-  let refreshToken = request.cookies.get("refresh_token")?.value;
+  let refreshToken = request.cookies.get("refresh_token")?.value ?? null;
+  console.log({refreshToken});
   let path = request.nextUrl.pathname;
-  let { passed, to } = await userValidation(path, accessToken, refreshToken);
+  let { passed, to } = await userValidation(path, refreshToken);
   let response = NextResponse.next();
-
   if (passed) return response;
 
   if (!passed) return Response.redirect(APP_CONFIG.webUrl + to);
@@ -27,35 +25,23 @@ export const config = {
   ],
 };
 
-async function userValidation(path, accessToken, refreshToken) {
-  let cookie = accessToken || refreshToken;
-  if (cookie && (path == "/login" || path == "/register"))
+async function userValidation(path, refreshToken) {
+  if (refreshToken && (path == "/login" || path == "/register"))
     return { passed: false, to: "/auth-reset" };
-  else if (!cookie && (path == "/login" || path == "/register"))
+  else if (!refreshToken && (path == "/login" || path == "/register"))
     return { passed: true, to: "/" };
-  if (!cookie) return { passed: false, to: `/login?redirect=${path}` };
+  if (!refreshToken) return { passed: false, to: `/login?redirect=${path}` };
+  const result = await verifyUser(refreshToken);
   if (path.startsWith("/admin")) {
-    try {
-      let result = await verifyUser(refreshToken);
       if (result?.role == "admin") return { passed: true, to: path };
       return { passed: false, to: "/auth-error" };
-    } catch (error) {
-      return { passed: false, to: "/auth-error" };
-    }
   }
 
-  try {
-    let result = await verifyUser(refreshToken);
     if (result.id) return { passed: true, to: path };
-
     return { passed: false, to: "/auth-reset" };
-  } catch (error) {
-    return { passed: false, to: "/auth-error" };
-  }
 }
 async function verifyUser(refreshToken) {
   try {
-    if(!refreshToken) return {};
     let token = refreshToken?.replace("bearer ", "");
     const result = await jwtVerify(token, secret, {
       algorithms: ["HS256"],
